42f10889810de70e0d6ef12ff18d2b8015032be0
fireyy <fireyy@gmail.com>
2022-03-04 10:43:30 +0800

    chore: 调整interface，移除多余字段

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,36 +1,18 @@
 export type AppItem = {
   name: string
   description: string
+  deviceType: string
   slug: string
   icon?: string
   archived?: boolean
-  channelCount?: number
   createdAt: string
   updatedAt: string
-  deletedAt?: string | null
   id: number
   lastPkgId?: string | null
   lastPkgSize?: string | null
   lastVersion?: string | null
   packagesCount?: number
   userId: number
-}
-
-export type ChannelItem = {
-  id: number
-  name: string
-  slug: string
-  appId?: number
-  packageName?: string
-  packagesCount?: number
-  bundleId?: string
-  deviceType: string
-  userId: number
-  password?: string
-  sort?: number
-  createdAt: string
-  updatedAt: string
-  deletedAt?: string | null
 }
 
 export type AppInfo = {
@@ -48,8 +30,6 @@ export type PackageItem = {
   appId: number
   name: string
   icon?: string
-  channelId: number
-  channelName: string
   bundleId?: string
   version?: string
   buildVersion?: string
@@ -57,7 +37,6 @@ export type PackageItem = {
   file?: string
   createdAt: string
   updatedAt: string
-  deletedAt: string | null
   size?: number
   userId: number
 }
