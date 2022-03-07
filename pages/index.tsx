2df45bc8d7ed8ceaad03ebb7ce13820b309eafb1
fireyy <fireyy@gmail.com>
2022-03-07 16:45:58 +0800

    feat: clean

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage, GetServerSideProps } from 'next'
 import Router from 'next/router'
-import { Button, Grid, Input, useTheme } from '@geist-ui/core'
+import { Button, Grid, Input, useTheme, Select } from '@geist-ui/core'
 import SearchIcon from '@geist-ui/icons/search'
 import CreateTeamIcon from '@geist-ui/icons/userPlus'
 import { AppItem } from '../interfaces'
@@ -20,16 +20,9 @@ const Home: NextPage<Props> = ({ data }) => {
       <Title value="Home" />
       <div className="page__content">
         <div className="actions-stack">
-          <Input
-            scale={1.25}
-            width="100%"
-            icon={<SearchIcon color={theme.palette.accents_5} />}
-            placeholder="Search..."
-          />
-          <Button auto type="secondary" marginLeft={1} onClick={() => Router.push('/apps/new')}>
+          <Button auto type="secondary" onClick={() => Router.push('/apps/new')}>
             New Project
           </Button>
-          <Button iconRight={<CreateTeamIcon />} marginLeft={1} px={0} width="48px" />
         </div>
         <Grid.Container gap={2} marginTop={1} justify="flex-start">
           {
@@ -60,18 +53,12 @@ const Home: NextPage<Props> = ({ data }) => {
           width: ${theme.layout.pageWidthWithMargin};
           max-width: 100%;
           margin: 0 auto;
-          padding: calc(${theme.layout.unit} * 2) ${theme.layout.pageMargin};
+          padding: ${theme.layout.unit} ${theme.layout.pageMargin};
           box-sizing: border-box;
         }
         .actions-stack {
           display: flex;
           width: 100%;
-        }
-        .actions-stack :global(.input-wrapper) {
-          background-color: ${theme.palette.background};
-        }
-        .actions-stack :global(input) {
-          font-size: 14px;
         }
       `}</style>
     </>
