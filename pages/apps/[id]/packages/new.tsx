70b744539c810c43ec18d9fc7753356e6469edd1
fireyy <fireyy@gmail.com>
2022-03-08 10:21:52 +0800

    fix: 限制文件类型选择

--- pages/apps/[id]/packages/new.tsx
+++ pages/apps/[id]/packages/new.tsx
@@ -1,17 +1,22 @@
 import React, { useEffect, useState, useRef } from 'react'
-import Router, { useRouter } from 'next/router'
+import { GetServerSideProps } from 'next'
+import Router from 'next/router'
 import { Input, Button, Text, Grid, Textarea, Display, Code, useInput } from '@geist-ui/core'
 import Upload from '@geist-ui/icons/upload'
-import { PackageItem } from 'interfaces'
+import { AppItem, PackageItem } from 'interfaces'
 import Title from 'components/title'
 import NavLink from 'components/nav-link'
+import DeviceType from 'components/device-type'
 
-const PackageNewPage: React.FC<unknown> = () => {
+type Props = {
+  app: AppItem
+}
+
+const PackageNewPage: React.FC<Props> = ({ app }) => {
   const [data, setData] = useState<PackageItem>(null)
   const {state: desc, setState: setDesc, bindings: descBindings} = useInput('')
   const [loading, setLoading] = useState(false)
   const hiddenFileInput = useRef(null)
-  const { query: { id = 0 } } = useRouter()
 
   const handleFile= async (event: any) => {
     if (event.target.files && event.target.files[0]) {
@@ -31,22 +36,23 @@ const PackageNewPage: React.FC<unknown> = () => {
   const handleSubmit = async () => {
     setLoading(true)
     if(desc) data.changelog = desc
-    await fetch(`http://localhost:3000/api/apps/${id}/packages`, {
+    await fetch(`http://localhost:3000/api/apps/${app.id}/packages`, {
       method: 'PUT',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(data),
     })
-    await Router.push(`/apps/${id}`)
+    await Router.push(`/apps/${app.id}`)
   }
 
   return (
     <>
       <Title value="New Package" />
-      <NavLink href={`/apps/${id}`}>Back</NavLink>
+      <NavLink href={`/apps/${app.id}`}>Back</NavLink>
       <Grid.Container gap={2} justify="center">
         <Grid xs={24}>
-          <Display caption="Upload ipa or apk">
-            <input type="file" ref={hiddenFileInput} onChange={handleFile} accept="*/*" style={{ display: 'none' }} />
+          <Display caption={`Upload ${app.deviceType === 'ios' ? '.ipa' : '.apk'} file`}>
+            <Text h4>{app.name} <DeviceType size={14} type={app.deviceType} /></Text>
+            <input type="file" ref={hiddenFileInput} onChange={handleFile} accept={app.deviceType === 'ios' ? '.ipa' : 'application/vnd.android.package-archive'} style={{ display: 'none' }} />
             <Button auto icon={<Upload />} onClick={() => hiddenFileInput.current.click()}>Upload</Button>
           </Display>
         </Grid>
@@ -66,6 +72,12 @@ const PackageNewPage: React.FC<unknown> = () => {
       </Grid.Container>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const res = await fetch(`http://localhost:3000/api/apps/${context.params.id}`)
+  const data = await res.json()
+  return { props: { app: data } }
 }
 
 export default PackageNewPage
