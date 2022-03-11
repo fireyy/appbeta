4d372266b6c103001defd01a0e10344d0417c9d5
fireyy <fireyy@gmail.com>
2022-03-11 17:28:27 +0800

    feat: user表增加创建和更新时间

---
+++ prisma/migrations/20220311091948_add_created_at_to_user/migration.sql
@@ -0,0 +1,19 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT,
+    "email" TEXT,
+    "emailVerified" DATETIME,
+    "image" TEXT,
+    "phone" TEXT,
+    "role" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+INSERT INTO "new_User" ("email", "emailVerified", "id", "image", "name", "phone", "role") SELECT "email", "emailVerified", "id", "image", "name", "phone", "role" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
