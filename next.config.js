4b2c4cd3b54fbd0aa5a688e08ea36e05bef13e88
fireyy <fireyy@gmail.com>
2022-03-14 14:10:31 +0800

    fix: phase is not defined

--- next.config.js
+++ next.config.js
@@ -2,16 +2,19 @@
 const path = require('path')
 const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
 
-const nextConfig = {
-  reactStrictMode: true,
-  serverRuntimeConfig: {
-    pkgPath: phase === PHASE_DEVELOPMENT_SERVER ? path.resolve(__dirname, 'public', 'downloads') : path.resolve(__dirname, 'downloads'),
-    iconPath: phase === PHASE_DEVELOPMENT_SERVER ? path.resolve(__dirname, 'public', 'icons') : path.resolve(__dirname, 'icons')
-  },
-  publicRuntimeConfig: {
-    baseUrl: process.env.NEXTAUTH_URL || 'http://localhost:3000',
-    staticFolder: '/'
+const nextConfig = async (phase, { defaultConfig }) => {
+  const nextConfig = {
+    reactStrictMode: true,
+    serverRuntimeConfig: {
+      pkgPath: phase === PHASE_DEVELOPMENT_SERVER ? path.resolve(__dirname, 'public', 'downloads') : path.resolve(__dirname, 'downloads'),
+      iconPath: phase === PHASE_DEVELOPMENT_SERVER ? path.resolve(__dirname, 'public', 'icons') : path.resolve(__dirname, 'icons')
+    },
+    publicRuntimeConfig: {
+      baseUrl: process.env.NEXTAUTH_URL || 'http://localhost:3000',
+      staticFolder: '/'
+    }
   }
+  return nextConfig
 }
 
 module.exports = nextConfig
